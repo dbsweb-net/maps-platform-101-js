b7d6e774f23ae6f7713d04c7de4a2525ccfbc313
dbsweb-net <sorin.munteanu@dbsweb.net>
2021-11-28 12:11:46 +0200

    commit to master

--- starter/src/app.js
+++ starter/src/app.js
@@ -13,3 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ import { Loader } from '@googlemaps/js-api-loader';
+ const apiOptions = {
+    apiKey: "AIzaSyCx_RMiJMjpN0qc_NkQQToud2k0v56JHA0"
+  }
+  const loader = new Loader(apiOptions);
+  loader.load().then(() => {
+    console.log('Maps JS API loaded');
+  });
+  function displayMap() {
+    const mapOptions = {
+      center: { lat: -33.860664, lng: 151.208138 },
+      zoom: 14
+    };
+    const mapDiv = document.getElementById('map');
+    const map = new google.maps.Map(mapDiv, mapOptions);
+    return map;
+  }
